477269d7f1572b3a2d92b11cc0f5cfb2811e1eac
Matvey <tok61599@gmail.com>
2024-12-09 12:52:45 +0300

    добавили игрока С АНИМАЦИЕЙ!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+
+
 pygame.init()
 screen = pygame.display.set_mode((950, 555))
 pygame.display.set_caption('PyGame')
@@ -7,13 +9,37 @@ icon = pygame.image.load('files/i.png')
 pygame.display.set_icon(icon)
 
 bg = pygame.image.load('files/background.png')
-player = pygame.image.load('files/left/1.png')
+
+# подгружаем ходьбу
+
+walk_left = [
+    pygame.image.load('files/left/1.png'),
+    pygame.image.load('files/left/2.png'),
+    pygame.image.load('files/left/3.png'),
+    pygame.image.load('files/left/4.png')
+]
+
+walk_right = [
+    pygame.image.load('files/right/1.png'),
+    pygame.image.load('files/right/2.png'),
+    pygame.image.load('files/right/3.png'),
+    pygame.image.load('files/right/4.png')
+]
+
+
+player_anim_count = 0
+
 
 running = True
 while running:
 
     screen.blit(bg, (0, 0))
-    screen.blit(player, (300, 400))
+    screen.blit(walk_right[player_anim_count], (300, 400))
+
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
 
 
 
