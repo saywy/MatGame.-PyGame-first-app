184d462249c7cc2dbbf07a176d667fdd0564c006
Matvey <tok61599@gmail.com>
2024-12-09 12:22:42 +0300

    добавил вывод изображения на экран выводится изображение (тоже, что стоит на иконке)

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pygame
 pygame.init()
 
 
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption('PyGame')
 
 icon = pygame.image.load('files/i.png')
+
 pygame.display.set_icon(icon)
 
 
@@ -15,15 +16,18 @@ square = pygame.Surface((100, 250))
 square.fill('Blue')
 
 myfont = pygame.font.Font('files/shadow-whisper-demo.regular.ttf', 40)
-text_surface = myfont.render('Matvey', False, 'White')
+text_surface = myfont.render('Matvey', True, 'White')
+
+player = pygame.image.load('files/i.png')
 
 running = True
-
 while running:
 
     screen.blit(square, (10, 0))
     screen.blit(text_surface, (300, 100))
     pygame.draw.circle(screen, 'Orange', (20, 20), 20)
+    screen.blit(player, (200, 200))
+
 
     pygame.display.update()
 
