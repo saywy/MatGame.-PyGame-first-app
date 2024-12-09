24445ee7574d8bff44eb25d12eca6f7b4ad08ace
Matvey <tok61599@gmail.com>
2024-12-09 12:29:26 +0300

    почистили код 1 стадия создания игры!

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import pygame
 
 pygame.init()
-
-screen = pygame.display.set_mode((700, 550))
+screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption('PyGame')
-
 icon = pygame.image.load('files/i.png')
-
 pygame.display.set_icon(icon)
-
-square = pygame.Surface((100, 250))
-square.fill('Blue')
-
-myfont = pygame.font.Font('files/shadow-whisper-demo.regular.ttf', 40)
-text_surface = myfont.render('Matvey', True, 'White')
 
 player = pygame.image.load('files/i.png')
 
 running = True
 while running:
 
-    screen.blit(square, (10, 0))
-    screen.blit(text_surface, (300, 100))
-    pygame.draw.circle(screen, 'Orange', (20, 20), 20)
     screen.blit(player, (200, 200))
-
     pygame.display.update()
 
     for event in pygame.event.get():
