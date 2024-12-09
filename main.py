c933aaa857f00c926604349c489fdb93362f90d7
Matvey <tok61599@gmail.com>
2024-12-09 12:25:01 +0300

    pep

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import pygame
 
 pygame.init()
 
-
 screen = pygame.display.set_mode((700, 550))
 pygame.display.set_caption('PyGame')
 
 icon = pygame.image.load('files/i.png')
 
 pygame.display.set_icon(icon)
-
-
 
 square = pygame.Surface((100, 250))
 square.fill('Blue')
@@ -27,7 +24,6 @@ while running:
     screen.blit(text_surface, (300, 100))
     pygame.draw.circle(screen, 'Orange', (20, 20), 20)
     screen.blit(player, (200, 200))
-
 
     pygame.display.update()
 
