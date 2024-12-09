39031d9d6223c632d06b3e309a53dd1d1605e867
Matvey <tok61599@gmail.com>
2024-12-09 12:38:45 +0300

    добавили игрока! без анимации

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ icon = pygame.image.load('files/i.png')
 pygame.display.set_icon(icon)
 
 bg = pygame.image.load('files/background.png')
+player = pygame.image.load('files/left/1.png')
 
 running = True
 while running:
 
     screen.blit(bg, (0, 0))
+    screen.blit(player, (300, 400))
+
+
+
     pygame.display.update()
 
     for event in pygame.event.get():
